59df61ef50b2a2b3d280368b89fb47c01a6ef616
pixelr <devi9334>
2018-09-01 13:51:42 +0530

    Fixed coded; add controller

---
+++ src/main/java/com/dhanbad/todoApp/Controller/TodoController.java
@@ -0,0 +1,12 @@
+package com.dhanbad.todoApp.Controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/locksmith")
+@Validated
+public class TodoController {
+
+}
--- src/main/java/com/dhanbad/todoApp/Model/todoappTodoTable.java
+++ src/main/java/com/dhanbad/todoApp/Model/todoappTodoTable.java
@@ -2,14 +2,18 @@ package com.dhanbad.todoApp.Model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
 import java.util.List;
 
 @Entity
+@Table(name = "todolist")
 public class todoappTodoTable {
+    @Id
     @Column(name = "user_id")
     private String userId;
     @Column(name = "todo_name")
     private String todoName;
     @Column(name = "todo_list")
-    private List<TodoItem> itemList;
+    private String itemList;
 }
